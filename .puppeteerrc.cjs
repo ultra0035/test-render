b681c02cc01065ba7957799aaf7b014bf56a8e37
Aaron Tatenda <aaronmutsvanga2@gmail.com>
2026-08-08 19:14:10 +0200

    Ready for Railway

--- .puppeteerrc.cjs
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    cacheDirectory: path.join(__dirname, ".cache", "puppeteer"),
-    chrome: {
-        skipDownload: false
-    }
-};
--- index.js
+++ index.js
@@ -2,28 +2,20 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    // This tells the bot to save your login session in a folder called 'sessions'
-    authStrategy: new LocalAuth({
-        dataPath: './sessions' 
-    }),
+    authStrategy: new LocalAuth({ dataPath: './sessions' }),
     puppeteer: {
         headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ],
-        // This is where Railway will look for the Chrome browser
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable'
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
 client.on('qr', (qr) => {
-    // This prints the QR code in the Railway logs so you can scan it
+    // This will show up in your Railway "View Logs" tab
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('Bot is logged in and ready!');
+    console.log('BOT IS ONLINE!');
 });
 
 client.initialize();
