d11d18b263b28d16e46587397c52c1d07bbd7cc1
Aaron Tatenda <aaronmutsvanga2@gmail.com>
2026-08-08 19:26:04 +0200

    Complete working setup

--- index.js
+++ index.js
@@ -2,20 +2,39 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth({ dataPath: './sessions' }),
+    authStrategy: new LocalAuth({
+        dataPath: './sessions' 
+    }),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ],
+        executablePath: '/usr/bin/google-chrome-stable',
     }
 });
 
 client.on('qr', (qr) => {
-    // This will show up in your Railway "View Logs" tab
+    console.log('SCAN THIS QR CODE IN YOUR RAILWAY LOGS:');
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('BOT IS ONLINE!');
+    console.log('SUCCESS: Client is ready!');
+});
+
+// Basic test command
+client.on('message', msg => {
+    if (msg.body === '!ping') {
+        msg.reply('pong');
+    }
 });
 
 client.initialize();
